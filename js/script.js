8d866a6ebd80c974dfcd4b1fe65151a090aa91cb
Itdunai <itdunai@yandex.ru>
2021-11-14 16:07:22 +0800

    Start

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// const answers = [];
+
+// answers[0] = prompt('Как выше имя?', '');
+// answers[1] = prompt('Как выше фамилия?', '');
+// answers[2] = prompt('Скок лет?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`);
