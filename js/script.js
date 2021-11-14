de312b51452d32c5a29c25a15a93aa0959cb9681
Itdunai <itdunai@yandex.ru>
2021-11-14 20:55:24 +0800

    Praktik 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// const num = 49;
+
+// (num == 50) ? console.log('Ok') : console.log('Error'); 
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const presonalMovieDB = {
@@ -10,16 +14,30 @@ const presonalMovieDB = {
         privat: false  
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('Насколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('Насколько оцените его?', '');
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('Насколько оцените его?', '');
 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        presonalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-presonalMovieDB.movies[a] = b;
-presonalMovieDB.movies[c] = d;
+if(presonalMovieDB.count < 10){
+    console.log('Мало посмотрел');
+} else if (presonalMovieDB.count >= 10 && presonalMovieDB.count < 30) {
+    console.log('Нормуль');
+} else if (presonalMovieDB.count > 30) {
+    console.log('Конченый');
+}
 
 console.log(presonalMovieDB);
+
+
 
 
 
